fd438da1c39cd2055227bd565cd05c927ee5bd75
Lorenzo Croce <lorenzo.croce.1@studenti.unipd.it>
2025-12-27 10:34:44 +0000

    feat: implement task routines

---
+++ include/task_routines.h
@@ -0,0 +1,11 @@
+#ifndef TASK_ROUTINES_H
+#define TASK_ROUTINES_H
+#include "task.h"
+
+void routines_init(void);
+
+inline const TaskType *routines_get_by_name(const char *name);
+
+const TaskType *routines_get_all(int *count);
+
+#endif
